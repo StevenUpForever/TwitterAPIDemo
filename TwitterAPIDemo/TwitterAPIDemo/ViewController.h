6b15cabfea1f0537d6756901c4f1de7cb2e27539
Chengzhi Jia <j.chengzhi1989@gmail.com>
2016-06-28 18:01:43 -0700

    Initial upload

---
+++ TwitterAPIDemo/TwitterAPIDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterAPIDemo
+//
+//  Created by Chengzhi Jia on 6/28/16.
+//  Copyright © 2016 ChengzhiJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TwitterAPIDemo/TwitterAPIDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TwitterAPIDemo
+//
+//  Created by Chengzhi Jia on 6/28/16.
+//  Copyright © 2016 ChengzhiJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
